8b5eeab242ccd6675e235ab35c421cd9a5ae3347
GITHub <silviofjesus@gmail.com>
2024-01-08 17:45:16 -0300

    Criando a conta.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int Conta;
+        int Agencia;
+        String Nome_do_cliente;
+        double Saldo = 1024;
+
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Olá, Digite o numero da Agencia: ");
+        Agencia = ler.nextInt();
+        System.out.println("Digite o número da Conta: ");
+        Conta = ler.nextInt();
+        System.out.println("Digite o Nome do cliente: ");
+        Nome_do_cliente = ler.next();
+        System.out.println("Ola " + Nome_do_cliente + " Obrigado por criar uma conta conosco, sua Agencia é: " + Agencia + " Conta: " + Conta + " e o seu Saldo " + Saldo + " ja esta disponivel para saque.");
+
     }
 }
